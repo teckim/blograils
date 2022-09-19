29a41d70b9b4ea3495416e522d88be4b035fdff3
BHDHakim <hakim.bhd@gmail.com>
2022-09-19 13:21:41 +0300

    Add User and Post controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+  before_action :fetch_author
+
+  def index
+    @posts = @author.posts
+  end
+
+  def show
+    @post = @author.posts.find(params[:id])
+  end
+
+  private
+
+  def fetch_author
+    @author = User.find(params[:user_id])
+  end
+end
