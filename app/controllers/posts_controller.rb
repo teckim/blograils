6b2df2a757af578aa2730c0a655c352d846b80bf
BHDHakim <hakim.bhd@gmail.com>
2022-09-26 15:10:41 +0100

    Fix n+1 issues

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :fetch_author, only: %i[index show]
 
   def index
-    @posts = @author ? @author.posts : Post.all
+    @posts = @author ? @author.posts.includes(:comments, :author) : Post.includes(:comments, :author).all
   end
 
   def show
