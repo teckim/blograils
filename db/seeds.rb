a57dc3fabc409ce22390bbb440151747fc9d27dd
BHDHakim <hakim.bhd@gmail.com>
2022-09-29 12:47:57 +0100

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 Like.destroy_all
 Comment.destroy_all
 Post.destroy_all
 User.destroy_all
 
 # User seeds
-user_1 = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-user_2 = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+user_1 = User.create(name: 'Tom', photo: 'https://xsgames.co/randomusers/avatar.php?g=male', bio: 'Teacher from Mexico.', email: 'tom@email.com', password: '123456789', password_confirmation: '123456789')
+user_2 = User.create(name: 'Lilly', photo: 'https://xsgames.co/randomusers/avatar.php?g=female', bio: 'Teacher from Poland.', email: 'lily@email.com', password: '123456789', password_confirmation: '123456789')
 
 p "Created #{User.count} Users"
 
