1a2986ee5a3cc5a0d724eb96d1b41a1222238958
BHDHakim <hakim.bhd@gmail.com>
2022-09-30 08:44:39 +0100

    Add admin? helper to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,10 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  def admin?
+    role == 'admin'
+  end
+
   def last_three_posts
     posts.last(3)
   end
