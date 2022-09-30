23320949f4257e516bc1a43260d9dbc31093a63e
BHDHakim <hakim.bhd@gmail.com>
2022-09-30 14:13:07 +0100

    Fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   before_action :update_allowed_parameters, if: :devise_controller?
 
-  rescue_from CanCan::AccessDenied do | exception |
+  rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
   end
 
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,16 +1,15 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can [:read, :create], Post
-    can [:read, :create], Comment
+    can %i[read create], Post
+    can %i[read create], Comment
     can :destroy, Post, author: user
     can :destroy, Comment, author: user
 
-    return unless user && user.admin?
+    return unless user&.admin?
+
     can :destroy, Post
     can :destroy, Comment
-   end
+  end
 end
